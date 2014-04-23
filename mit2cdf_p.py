c36240f08a0d64842e0f69be9db49bd95664888c
Nikolay Koldunov <koldunovn@gmail.com>
2014-04-23 12:23:08 +0200

    remove ipcluster start/stop related code

--- mit2cdf_p.py
+++ mit2cdf_p.py
@@ -16,13 +16,13 @@ import cvmit_tave
 import time 
 
 #os.system('ipcluster start -n 4 &')
-def launch_cluster(n=2, *args):
-    return Popen(['ipcluster', 'start', '-n', str(n)] + list(args), stdout=PIPE, stderr=PIPE)
+#def launch_cluster(n=2, *args):
+#    return Popen(['ipcluster', 'start', '-n', str(n)] + list(args), stdout=PIPE, stderr=PIPE)
 
-def stop_cluster():
-    return Popen(['ipcluster', 'stop'], stdout=PIPE, stderr=PIPE)
+#def stop_cluster():
+#    return Popen(['ipcluster', 'stop'], stdout=PIPE, stderr=PIPE)
 
-launch_cluster()
+#launch_cluster()
 
 #time.sleep(2.)
 
@@ -37,6 +37,6 @@ for i in range(len(fileList)):
 
 dv.map_sync(cvmit_tave.cnv2netcdf, fileList)
 
-stop_cluster()
+#stop_cluster()
 
 #os.system('ipcluster stop')
